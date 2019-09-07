523f56e6221126cc6b587f28bf4c82030d2caf53
Fallrainy <nomeqc@gmail.com>
2019-09-07 11:19:04 +0800

    update install.sh

--- v2/install.sh
+++ v2/install.sh
@@ -1,31 +1,64 @@
 #!/bin/bash
 
+#######color code########
+RED="31m"      # Error message
+GREEN="32m"    # Success message
+YELLOW="33m"   # Warning message
+BLUE="36m"     # Info message
+
+colorEcho(){
+    COLOR=$1
+    echo -e "\033[${COLOR}${@:2}\033[0m"
+}
+
 wget -O go.sh https://install.direct/go.sh && sh go.sh
 (curl -L -s  https://raw.githubusercontent.com/Nomeqc/scripts/master/v2/config.json)>/etc/v2ray/config.json
 service v2ray restart
 
-echo "在网站的nginx配置文件中加入以下配置："
-cat << EOF
+echo -e "\n在网站的nginx配置文件中加入以下配置："
+conf=`cat << EOF
  location /v2 {
         proxy_redirect off;
         proxy_pass http://127.0.0.1:44222;
         proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Upgrade \\$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header Host \$http_host;
+        proxy_set_header Host \\$http_host;
     }
-    
-EOF
+EOF`
+colorEcho ${BLUE} "${conf}\n"
 
-echo "修改nginx配置，开启拦截反向代理错误，在http{}中插入："
-cat << EOF
+echo  -e "修改nginx配置，开启拦截反向代理错误，在http{}中插入："
+conf=`cat << EOF
  proxy_intercept_errors on;
- 
-EOF
+EOF`
+colorEcho ${BLUE} "${conf}\n"
 
-echo "在网站nginx配置文件中加入400错误处理："
-cat << EOF
+
+echo -e "在网站nginx配置文件中加入400错误处理："
+conf=`cat << EOF
  error_page 400 /index.php;
- 
-EOF
+EOF`
+colorEcho ${BLUE} "${conf}\n"
 
+echo -e "v2ray客户端配置："
+echo -n "端口(port)："
+colorEcho ${BLUE} "443"
+
+echo -n "用户ID(id)："
+colorEcho ${BLUE} "2d2f5648-2646-4722-b31f-fd1369023e37"
+
+echo -n "额外ID(alertId)："
+colorEcho ${BLUE} "32"
+
+echo -n "加密方式(security)："
+colorEcho ${BLUE} "auto"
+
+echo -n "传输协议(network)："
+colorEcho ${BLUE} "ws"
+
+echo -n "路径(path)："
+colorEcho ${BLUE} "v2"
+
+echo -n "底层传输安全："
+colorEcho ${BLUE} "tls\n"
