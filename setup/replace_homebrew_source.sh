03e9e9f6ab7f60ac6a34ed11845e461f093396d7
Nomeqc <nomeqc@gmail.com>
2020-02-07 12:40:53 +0800

    Update replace_homebrew_source.sh

--- setup/replace_homebrew_source.sh
+++ setup/replace_homebrew_source.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 # 替换brew.git:
 cd "$(brew --repo)"
-# 中国科大:
-git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
-
-#替换homebrew-core.git:
+git remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+# 替换homebrew-core.git:
 cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-# 中国科大:
-git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
-
+git remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+# 应用生效
+brew update
 # 替换homebrew-bottles:
-# 中国科大:
-echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles' >> ~/.bash_profile
+echo -e '\nexport HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.bash_profile
 source ~/.bash_profile
