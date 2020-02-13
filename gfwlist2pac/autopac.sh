c20ee9bf53aa56675fe24410fe5d3c8d17104937
Nomeqc <nomeqc@gmail.com>
2020-02-13 19:19:39 +0800

    Add files via upload

---
+++ gfwlist2pac/autopac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GFWLIST=https://vv.fallrainy.top/gfwrule/data/list.txt
+PROXY=127.0.0.1:10808
+
+cd `dirname "${BASH_SOURCE[0]}"`
+echo "Downloading gfwlist from $GFWLIST"
+curl "$GFWLIST" --socks5-hostname "$PROXY" > /tmp/gfwlist.txt
+/usr/local/bin/gfwlist2pac \
+    --input /tmp/gfwlist.txt \
+    --file proxy.pac \
+    --proxy "SOCKS5 $PROXY; SOCKS $PROXY; DIRECT" \
+    --user-rule user_rule.txt \
+    --precise
+
+rm -f /tmp/gfwlist.txt
