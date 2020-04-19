ada80c8d455f3267f8cb1a97139bb8eabd253618
Nomeqc <nomeqc@gmail.com>
2020-04-20 01:51:57 +0800

    Update and rename m3u8.py to m3u8-down.py
    - 修复片段失败导致卡死
    - 修复下载加密的片段key缓存无效

--- python/m3u8_download/m3u8-down.py
+++ python/m3u8_download/m3u8-down.py
@@ -32,7 +32,6 @@ class Downloader:
         self.key_map = {}
         self.tmp_filename = ''
         self.m3u8_obj = None
-        self.is_enc = False
 
     def _get_http_session(self, pool_connections, pool_maxsize, max_retries):
             session = requests.Session()
@@ -62,15 +61,11 @@ class Downloader:
         if len(m3u8_obj.segments) > 0:
             if self.dir and not os.path.isdir(self.dir):
                 os.makedirs(self.dir)
-
             #创建临时的文件夹，用于存放片段
             self.tmp_dir = os.path.join(self.dir, hashlib.md5(m3u8_url).hexdigest())
             if not os.path.isdir(self.tmp_dir):
                 os.makedirs(self.tmp_dir)
-            self.record_filepath = os.path.join(self.tmp_dir, 'record.json')
-
-            self.is_enc = bool(m3u8_obj.keys[-1])
-
+            # self.record_filepath = os.path.join(self.tmp_dir, 'record.json')
             m3u8_obj = zip(m3u8_obj.segments, [n for n in xrange(len(m3u8_obj.segments))])
 
             if m3u8_obj:
@@ -98,12 +93,13 @@ class Downloader:
             sys.exit()
 
     def _worker(self, ts_tuple):
-        index = ts_tuple[1]
         url = ts_tuple[0].uri
+        index = ts_tuple[1]
         retry = self.retry
 
         while retry:
             try:
+                # print('下载：' + url)
                 r = self.session.get(url, timeout=20)
                 if r.ok:
                     file_name = url.split('/')[-1].split('?')[0]
@@ -120,34 +116,47 @@ class Downloader:
                     if(len(self.succed) == self.ts_total):
                         print('')
                     return
+                else:
+                    retry -= 1
+                    print('not ok [FAIL]%s' % url)
             except:
                 retry -= 1
-        print('[FAIL]%s' % url)
+                print('exception [FAIL]%s' % url)
+        print('append failed url:' + url)
         self.failed.append((url, index))
 
     def _join_file(self):
         index = 0
         outfile = ''
         while index < self.ts_total:
+            # print('join:' + str(index))
             if len(self.succed) == self.ts_total:
                 s = "\r视频合并中 [{}/{}]".format(str(index + 1), str(self.ts_total))   #\r表示回车但是不换行，利用这个原理进行百分比的刷新
                 sys.stdout.write(s)       #向标准输出终端写内容
                 sys.stdout.flush()        #立即将缓存的内容刷新到标准输出
             file_name = self.succed.get(index, '')
+            # print "keys : %s" %  self.succed.keys()
             if file_name:
                 infile_path = os.path.join(self.tmp_dir, file_name)
-                if self.is_enc:
-                    seg = self.m3u8_obj.segments[index]
-                    # print('seg.key:' + str(seg.key))
+
+                seg = self.m3u8_obj.segments[index]
+
+                # 如果有key则说明片段加密了
+                is_enc = bool(seg.key)
+                if is_enc:
                     if seg.key.iv:
                         iv = '{:032x}'.format(int(str(seg.key.iv), 16))
                     else:
                         iv = '{:032x}'.format(int(str(index), 16))
-                    if not self.key_map.get(seg.uri, ''):
-                        resp = requests.get(seg.key.uri)
+                    
+                    key_uri = seg.key.uri
+                    key_content = self.key_map.get(key_uri, '')
+                    if not key_content:
+                        resp = requests.get(key_uri)
                         resp.encoding = 'utf-8'
-                        self.key_map[seg.uri] = resp.content.encode('hex')
-                    self._decrypt(infile_path, infile_path + '.dec', iv, self.key_map[seg.uri])
+                        key_content = resp.content.encode('hex')
+                        self.key_map[key_uri] = key_content
+                    self._decrypt(infile_path, infile_path + '.dec', iv, key_content)
                     infile_path = infile_path + '.dec'
                 if not outfile:
                     outfile = open(os.path.join(self.tmp_dir, self.tmp_filename), 'wb')
@@ -155,7 +164,7 @@ class Downloader:
                 outfile.write(infile.read())
                 infile.close()
                 os.remove(os.path.join(self.tmp_dir, file_name))
-                if self.is_enc:
+                if is_enc:
                     os.remove(infile_path)
                 index += 1
             else:
@@ -179,7 +188,7 @@ if __name__ == '__main__':
         print('格式：./m3u8.py [m3u8_url] [saved_dir] [saved_filename]')
         print('示例：./m3u8.py http://example.com/exp.m3u8 /home/video example.ts')
         sys.exit()
-    downloader = Downloader(10)
+    downloader = Downloader(50)
     print('下载 ' + m3u8_url)
     downloader.run(m3u8_url, saved_dir)
 
@@ -194,3 +203,4 @@ if __name__ == '__main__':
 
     # 删除临时目录
     shutil.rmtree(downloader.tmp_dir)
+
