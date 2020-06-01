d55ccc7ca067b62686d697e0debeac62c2d1e489
Nomeqc <nomeqc@gmail.com>
2020-06-01 10:14:33 +0800

    Update youku-iqiyi-qq-remove-watermark.js

--- tamper-monkey/youku-iqiyi-qq-remove-watermark.js
+++ tamper-monkey/youku-iqiyi-qq-remove-watermark.js
@@ -12,38 +12,18 @@
 
 var HostName = document.location.hostname;
 (()=>{
-
-//         switch(HostName) {
-//             case "v.qq.com":
-//                 setInterval(()=>{
-//                     //腾讯视频LOGO水印
-//                     document.querySelector(".txp_waterMark_pic") && document.querySelector(".txp_waterMark_pic").remove();
-//                 },1000*0.5);
-//                 break;
-//             case "v.youku.com":
-//                 setInterval(()=>{
-//                 //优酷“极清观影
-//                 document.querySelector(".js-top-icon") && document.querySelector(".js-top-icon").remove();
-//                 //优酷视频LOGO水印
-//                 document.querySelector(".youku-layer-logo") && document.querySelector(".youku-layer-logo").remove();
-//                 },1000*0.5);
-//                 break;
-//           case "www.iqiyi.com":
-//                 setInterval(()=>{
-//                   //爱奇艺LOGO水印
-//                   //$(".iqp-logo-box").remove();//为什么这样就不行呢？
-//                   document.querySelectorAll(".iqp-logo-box").forEach(function(item,index,arr){item.style.display='none';});
-//                 },1000*0.5);
-//             break;
-//         }
-
     var observer = new MutationObserver(function(mutations, observer) {
         for (var mutation of mutations) {
             let target = mutation.target;
-            switch(HostName) {
+            switch (HostName) {
             case "v.qq.com":
                 //腾讯视频LOGO水印
                 target.querySelector(".txp_waterMark_pic") && target.querySelector(".txp_waterMark_pic").remove();
+                //去除广告
+                target.querySelectorAll(".site_board_ads").forEach(function(item, index, arr) {
+                    item.remove();
+                });
+
                 break;
             case "v.youku.com":
                 //优酷“极清观影
@@ -51,10 +31,18 @@ var HostName = document.location.hostname;
                 //优酷视频LOGO水印
                 target.querySelector(".youku-layer-logo") && target.querySelector(".youku-layer-logo").remove();
                 break;
-          case "www.iqiyi.com":
-                target.querySelectorAll(".iqp-logo-box").forEach(function(item,index,arr){item.style.display='none';});
+            case "www.iqiyi.com":
+                //去除广告
+                target.querySelectorAll("[data-adzone]").forEach(function(item, index, arr) {
+                    item.remove();
+                });
+                //去除logo
+                target.querySelectorAll(".iqp-logo-box").forEach(function(item, index, arr) {
+                    item.style.display = 'none';
+                });
                 break;
-        }
+            }
+
         }
     });
 
@@ -63,6 +51,5 @@ var HostName = document.location.hostname;
         childList: true,
         subtree: true
     });
-
 
 })();
